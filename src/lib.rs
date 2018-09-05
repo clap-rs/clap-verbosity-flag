579c91629da3c514ee07b4f95536e0aaa2d1d7e7
Manuel Thomassen <thom@ssen.codes>
2018-09-05 02:19:04 +0200

    manually impl StructOpt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 extern crate log;
-#[macro_use]
 extern crate structopt;
 
 use log::Level;
+#[allow(unused)]
+use structopt::clap::{App, Arg, ArgMatches};
+use structopt::StructOpt;
+
+#[cfg(not(any(feature = "log-level", feature = "verbosity", feature = "quietness")))]
+compile_error!("at least one of the clap-verbosity-flag features needs to be enabled: `log-level`, `verbosity`, `quietness`");
 
 const DEFAULT_VERBOSITY: u8 = 3;
 
-#[cfg(feature = "log-level")]
+#[allow(unused)]
 const VERBOSITY_LEVELS: &'static [&'static str] =
     &["quiet", "error", "warn", "info", "debug", "trace"];
 
@@ -30,81 +35,105 @@ const VERBOSITY_LEVELS: &'static [&'static str] =
 /// #
 /// # fn main() {}
 /// ```
-#[derive(StructOpt, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct Verbosity {
-    /// Pass many times for less log output. see `--log-level`
-    #[cfg(feature = "log-level")]
-    #[structopt(
-        short = "q", long = "quiet", group = "clap_verbosity_flag", parse(from_occurrences)
-    )]
-    quietness: u8,
-
-    /// Pass many times for more log output. see `--log-level`
-    #[cfg(feature = "log-level")]
-    #[structopt(
-        short = "v", long = "verbose", group = "clap_verbosity_flag", parse(from_occurrences)
-    )]
-    verbosity: u8,
-
-    /// Pass many times for less log output
-    ///
-    /// By default, it'll report errors, warnings and infos.
-    /// Passing `-q` one time disables infos, `-qq` disables warnings,
-    /// `-qqq` disables errors and will print nothing,
-    #[cfg(not(feature = "log-level"))]
-    #[structopt(
-        short = "q", long = "quiet", group = "clap_verbosity_flag", parse(from_occurrences)
-    )]
-    quietness: u8,
-
-    /// Pass many times for more log output
-    ///
-    /// By default, it'll report errors, warnings and infos.
-    /// Passing `-v` one time also prints debug, `-vv` enables trace logging.
-    #[cfg(not(feature = "log-level"))]
-    #[structopt(
-        short = "v", long = "verbose", group = "clap_verbosity_flag", parse(from_occurrences)
-    )]
-    verbosity: u8,
-
-    /// Set log level. By default it is info [possible values: quiet, error,
-    /// warn, info, debug, trace]
-    ///
-    /// Alternatively It's possible to use `-v`, `-vv` to increase and `-q`,
-    /// `-qq` etc. to decrease the log level.
-    #[cfg(feature = "log-level")]
-    #[structopt(
-        long = "log-level",
-        group = "clap_verbosity_flag",
-        raw(possible_values = "VERBOSITY_LEVELS", hide_possible_values = "true")
-    )]
-    level: Option<String>,
+    level: Option<Level>,
 }
 
-impl Verbosity {
-    /// Get the log level.
-    pub fn log_level(&self) -> Option<Level> {
+impl StructOpt for Verbosity {
+    fn clap<'a, 'b>() -> App<'a, 'b> {
+        let app = App::new("clap-verbosity-flag");
+        Self::augment_clap(app)
+    }
+    fn from_clap(matches: &ArgMatches) -> Self {
+        let verbosity = DEFAULT_VERBOSITY;
+        #[cfg(feature = "quietness")]
+        let verbosity = verbosity.saturating_sub(matches.occurrences_of("quietness") as u8);
+        #[cfg(feature = "verbosity")]
+        let verbosity = verbosity.saturating_add(matches.occurrences_of("verbosity") as u8);
         #[cfg(feature = "log-level")]
-        let verbosity = if let Some(level) = self.level.as_ref() {
-            VERBOSITY_LEVELS.iter().position(|l| l == level).unwrap() as u8
+        let verbosity = if let Some(level_str) = matches.value_of("level").as_ref() {
+            VERBOSITY_LEVELS
+                .iter()
+                .position(|l| l == level_str)
+                .unwrap() as u8
         } else {
-            self.verbosity_quietness()
+            verbosity
         };
-        #[cfg(not(feature = "log-level"))]
-        let verbosity = self.verbosity_quietness();
-
-        match verbosity {
+        let level = match verbosity {
             0 => None,
             1 => Some(Level::Error),
             2 => Some(Level::Warn),
             3 => Some(Level::Info),
             4 => Some(Level::Debug),
             _ => Some(Level::Trace),
+        };
+        Verbosity { level: level }
+    }
+}
+
+impl Verbosity {
+    pub fn augment_clap<'a, 'b>(app: App<'a, 'b>) -> App<'a, 'b> {
+        {
+            #[cfg(feature = "quietness")]
+            let app = app.arg({
+                let arg = Arg::with_name("quietness")
+                    .group("clap_verbosity_flag")
+                    .takes_value(false)
+                    .multiple(true)
+                    .short("q")
+                    .long("quiet");
+                #[cfg(feature = "log-level")]
+                let arg = arg.help("Pass many times for less log output. see `--log-level`");
+                #[cfg(not(feature = "log-level"))]
+                let arg = arg.help("Pass many times for less log output.\n\nBy default, it'll report errors, warnings and infos. Passing `-q` one time disables infos, `-qq` disables warnings, `-qqq` disables errors and will print nothing.");
+                arg
+            });
+            #[cfg(feature = "verbosity")]
+            let app = app.arg({
+                let arg = Arg::with_name("verbosity")
+                    .group("clap_verbosity_flag")
+                    .takes_value(false)
+                    .multiple(true)
+                    .short("v")
+                    .long("verbose");
+                #[cfg(feature = "log-level")]
+                let arg = arg.help("Pass many times for more log output. see `--log-level`");
+                #[cfg(not(feature = "log-level"))]
+                let arg = arg.help("Pass many times for more log output.\n\nBy default, it'll report errors, warnings and infos. Passing `-v` one time also prints debug, `-vv` enables trace logging.");
+                arg
+            });
+            #[cfg(feature = "log-level")]
+            let app = app.arg({
+                let arg = Arg::with_name("level")
+                    .group("clap_verbosity_flag")
+                    .takes_value(true)
+                    .multiple(false)
+                    .long("log-level")
+                    .possible_values(VERBOSITY_LEVELS)
+                    .hide_possible_values(true);
+                #[cfg(not(all(feature = "verbosity", feature = "quietness")))]
+                let arg = arg.help("Set log level. [default: info, possible values: quiet, error, warn, info, debug, trace]");
+                #[cfg(all(not(feature = "verbosity"), feature = "quietness"))]
+                let arg = arg.help("Set log level. [default: info, possible values: quiet, error, warn, info, debug, trace]\n\nAlternatively It\'s possible to use `-q`, `-qq` etc. to decrease the log level.");
+                #[cfg(all(feature = "verbosity", not(feature = "quietness")))]
+                let arg = arg.help("Set log level. [default: info, possible values: quiet, error, warn, info, debug, trace]\n\nAlternatively It\'s possible to use `-v`, `-vv` to increase the log level.");
+                #[cfg(all(feature = "verbosity", feature = "quietness"))]
+                let arg = arg.help("Set log level. [default: info, possible values: quiet, error, warn, info, debug, trace]\n\nAlternatively It\'s possible to use `-v`, `-vv` to increase and `-q`, `-qq` etc. to decrease the log level.");
+                arg
+            });
+            app
         }
     }
+    pub fn is_subcommand() -> bool {
+        false
+    }
+}
 
-    fn verbosity_quietness(&self) -> u8 {
-        (DEFAULT_VERBOSITY + self.verbosity).saturating_sub(self.quietness)
+impl Verbosity {
+    /// Get the log level.
+    pub fn log_level(&self) -> Option<Level> {
+        self.level
     }
 }
 

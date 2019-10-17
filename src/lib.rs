4a42a3af8ce0022d561615ae61945208f301da63
Ed Page <eopage@gmail.com>
2019-10-16 20:19:47 -0600

    feat: Configurable default level

--- src/lib.rs
+++ src/lib.rs
@@ -29,26 +29,49 @@ pub struct Verbosity {
     /// Pass many times for less log output
     #[structopt(long, short = "q", parse(from_occurrences), conflicts_with = "verbose")]
     quiet: i8,
+
+    #[structopt(skip)]
+    default: i8,
 }
 
 impl Verbosity {
+    /// Change the default level.
+    pub fn set_default(&mut self, level: Level) {
+        self.default = level_value(level);
+    }
+
     /// Get the log level.
     pub fn log_level(&self) -> Level {
-        let verbosity = 0 - self.quiet + self.verbose;
-
-        match verbosity {
-            std::i8::MIN..=0 => log::Level::Error,
-            1 => log::Level::Warn,
-            2 => log::Level::Info,
-            3 => log::Level::Debug,
-            4..=std::i8::MAX => log::Level::Trace,
-        }
+        level_enum(self.verbosity())
     }
 
     /// If the user requested complete silence (i.e. not just no-logging).
     pub fn is_silent(&self) -> bool {
-        let verbosity = 0 - self.quiet + self.verbose;
-        verbosity < 0
+        self.verbosity() < 0
+    }
+
+    fn verbosity(&self) -> i8 {
+        self.default - self.quiet + self.verbose
+    }
+}
+
+fn level_value(level: Level) -> i8 {
+    match level {
+        log::Level::Error => 0,
+        log::Level::Warn => 1,
+        log::Level::Info => 2,
+        log::Level::Debug => 3,
+        log::Level::Trace => 4,
+    }
+}
+
+fn level_enum(verbosity: i8) -> Level {
+    match verbosity {
+        std::i8::MIN..=0 => log::Level::Error,
+        1 => log::Level::Warn,
+        2 => log::Level::Info,
+        3 => log::Level::Debug,
+        4..=std::i8::MAX => log::Level::Trace,
     }
 }
 

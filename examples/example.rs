3aacc8ce60d00115388e8e3347af7a496112f634
Ed Page <eopage@gmail.com>
2022-06-13 11:41:40 -0500

    docs: Include tracing example

--- examples/example.rs
+++ examples/example.rs
@@ -14,4 +14,10 @@ fn main() {
     env_logger::Builder::new()
         .filter_level(cli.verbose.log_level_filter())
         .init();
+
+    log::error!("Engines exploded");
+    log::warn!("Engines smoking");
+    log::info!("Engines exist");
+    log::debug!("Engine temperature is 200 degrees");
+    log::trace!("Engine subsection is 300 degrees");
 }

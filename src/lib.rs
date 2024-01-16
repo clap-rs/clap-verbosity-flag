f3867c20afa5657ca4ee3e7f1a8e7972caa47aa1
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-01-16 21:25:02 +0100

    fix: remove comment that shows up in clap help text (#86)
    Fixes #85

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,7 @@ pub use log::LevelFilter;
 
 /// Logging flags to `#[command(flatten)]` into your CLI
 #[derive(clap::Args, Debug, Clone, Default)]
+#[command(about = None, long_about = None)]
 pub struct Verbosity<L: LogLevel = ErrorLevel> {
     #[arg(
         long,

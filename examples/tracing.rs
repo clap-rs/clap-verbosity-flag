1ba7d0354f396c0051037e7dfbc2b699129e4bb7
Josh McKinney <joshka@users.noreply.github.com>
2024-11-13 23:18:41 -0800

    docs: Update tracing example to use tracing feature
    Previously the example code was using the `tracing-log` crate to
    integrate with the `tracing` ecosystem. This crate is no longer
    necessary as `tracing` now provides implementations of the `LogLevel`
    trait that expose the `tracing` log levels instead of the `log` log
    levels.

--- examples/tracing.rs
+++ examples/tracing.rs
@@ -1,19 +1,18 @@
 use clap::Parser;
-use clap_verbosity_flag::Verbosity;
-use tracing_log::AsTrace;
+use clap_verbosity_flag::{tracing::ErrorLevel, Verbosity};
 
 /// Foo
 #[derive(Debug, Parser)]
 struct Cli {
     #[command(flatten)]
-    verbose: Verbosity,
+    verbose: Verbosity<ErrorLevel>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     tracing_subscriber::fmt()
-        .with_max_level(cli.verbose.log_level_filter().as_trace())
+        .with_max_level(cli.verbose.log_level_filter())
         .init();
 
     tracing::error!("Engines exploded");
